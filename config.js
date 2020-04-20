7defc04170d2592b5628787e1f4c333a3fb36f76
ajboni <mail@aboni.dev>
2020-04-20 18:44:16 -0300

    Crawler now deletes dst files when deleted

--- config.js
+++ config.js
@@ -1,31 +1,31 @@
 const config = {
-  // Folder where src docs are located.
-  SRC_DOCS_FOLDER: "docs",
+	// Folder where src docs are located.
+	SRC_DOCS_FOLDER: "docs",
 
-  // Folder where dist html docs are located. Relative to ./Public
-  DIST_DOCS_FOLDER: "docs",
+	// Folder where dist html docs will be located. Relative to ./Public
+	DIST_DOCS_FOLDER: "docs",
 
-  // ID for the default language. It will be accesible without any prefix in the slug.
-  DEFAULT_LANGUAGE_ID: "eng",
+	// ID for the default language. It will be accesible without any prefix in the slug.
+	DEFAULT_LANGUAGE_ID: "eng",
 
-  // Available languages
-  LANGUAGES: [
-    { id: "eng", caption: "English" },
-    { id: "spa", caption: "Spanish" },
-  ],
+	// Available languages
+	LANGUAGES: [
+		{ id: "eng", caption: "English" },
+		{ id: "spa", caption: "Spanish" },
+	],
 
-  // Should we show the default language doc if the localized version cannot be found ?
-  FALLBACK_TO_DEFAULT_LANGUAGE: true,
+	// Should we show the default language doc if the localized version cannot be found ?
+	FALLBACK_TO_DEFAULT_LANGUAGE: true,
 
-  // Prefix of filename that will be stripped out. Only used for sorting purposes. Anything before it, will be deleted from the doc title.
-  // AABA__HelloWorld.md
-  // AACA__Bye.md
-  SORT_PREFIX: "__",
+	// Prefix of filename that will be stripped out. Only used for sorting purposes. Anything before it, will be deleted from the doc title.
+	// AABA__HelloWorld.md
+	// AACA__Bye.md
+	SORT_PREFIX: "__",
 
-  // Suffix that separates the slug from the language modifier. It will be also be deleted fropm the title.
-  // AABA_HelloWorld.md    => Default Language
-  // AABA_HelloWorld-spa.md    => Spanish Language
-  LANGUAGE_SUFFIX: "-",
+	// Suffix that separates the slug from the language modifier. It will be also be deleted fropm the title.
+	// AABA_HelloWorld.md    => Default Language
+	// AABA_HelloWorld-spa.md    => Spanish Language
+	LANGUAGE_SUFFIX: "-",
 };
 
 module.exports.config = config;
--- crawler/crawl.js
+++ crawler/crawl.js
@@ -19,62 +19,76 @@ fs.emptyDirSync(Path.join("public", config.DIST_DOCS_FOLDER));
 
 // Initialize watcher.
 const watcher = chokidar.watch(Path.join(config.SRC_DOCS_FOLDER, "/**/*.md"), {
-  ignored: /(^|[\/\\])\../, // ignore dotfiles
-  persistent: true,
+	ignored: /(^|[\/\\])\../, // ignore dotfiles
+	persistent: true,
 });
 
 function processDocuments(path, verbose = false) {
-  // Generate Slug
-  const slug = slugify(path, config.SRC_DOCS_FOLDER);
+	// Generate Slug
+	const slug = slugify(path, config.SRC_DOCS_FOLDER);
 
-  // Parse file and convert to html.
-  const content = htmlfy(path);
+	// Parse file and convert to html.
+	const content = htmlfy(path);
 
-  // Create files in public folder
-  const file = writeFile(
-    content,
-    Path.join(config.DIST_DOCS_FOLDER, slug),
-    ".html"
-  );
+	// Create files in public folder
+	const file = writeFile(
+		content,
+		Path.join(slug),
+		".html"
+	);
 
-  // Fill slugs map with slug => htmlFile info
+	// Fill slugs map with slug => htmlFile info
 
-  if (!slug || !file) {
-    log(`${error(path + " => ERROR")}`);
-  } else {
-    const result = {
-      srcPath: path,
-      dstPath: file,
-      slug: slug,
-    };
-    docs.push(result);
-    if (verbose) {
-      log(success("[OK] REBUILD FILE"));
-      log("\r");
-      log(columnify(new Array(result)));
-      log("\r");
-      log(warning("Watching files..."));
-      log("\r");
-    }
-  }
+	if (!slug || !file) {
+		log(`${error(path + " => ERROR")}`);
+	} else {
+		const result = {
+			srcPath: path,
+			dstPath: file,
+			slug: slug,
+		};
+		docs.push(result);
+		if (verbose) {
+			log(success("[OK] REBUILD FILE"));
+			log("\r");
+			log(columnify(new Array(result)));
+			log("\r");
+			log(warning("Watching files..."));
+			log("\r");
+		}
+	}
+}
+
+function onDeleteFile(path) {
+	const slug = slugify(path, config.SRC_DOCS_FOLDER);
+	const htmlFile = Path.join('public', slug + ".html");
+	try {
+		fs.unlinkSync(htmlFile)
+		log(success(`[OK] FILE ${htmlFile} DELETED`));
+		log("\r");
+	} catch (err) {
+		log(error(`[ERROR] FILE ${htmlFile} NOT FOUND`));
+		log("\r");
+	}
+
 }
 
 // Something to use when events are received.
 // Add event listeners.
 watcher
-  .on("add", (path) => processDocuments(path))
-  .on("change", (path) => processDocuments(path, true))
-  .on("unlink", (path) => log(`File ${path} has been removed`))
-  .on("ready", onCrawlReady);
+	.on("add", (path) => processDocuments(path))
+	.on("change", (path) => processDocuments(path, true))
+	.on("unlink", (path) => onDeleteFile(path))
+	.on("ready", onCrawlReady);
 
 log(`Crawling folder:  ${warning(config.SRC_DOCS_FOLDER)} for .md files...`);
 log("\r");
 
 function onCrawlReady() {
-  log(columnify(docs));
-  log("\r");
-  log(success("[OK] CRAWLING DONE..."));
-  log("\r");
-  log(warning("Watching files..."));
-  log("\r");
+	log(columnify(docs));
+	log("\r");
+	log(success("[OK] CRAWLING DONE..."));
+	log("\r");
+	log(warning("Watching files..."));
+	log("\r");
 }
